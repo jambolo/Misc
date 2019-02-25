54034427c34711ea81c4787e386d8ae1cede9d06
John Bolton <odolvlobo-github@yahoo.com>
2019-02-24 16:11:43 -0800

    Added Deferred class

---
+++ include/Misc/Deferred.h
@@ -0,0 +1,77 @@
+#pragma once
+#pragma once
+
+#if !defined(MISC_DEFERRED_H)
+#define MISC_DEFERRED_H
+
+//! A type that knows if it has ever been given a value.
+//!
+//! @param  T    The type of the value
+//!
+//! @note   T must be CopyConstructible, CopyAssignable and DefaultConstructible
+template <class T>
+class Deferred
+{
+public:
+    //! Constructor.
+    Deferred() = default;
+
+    //! Constructor.
+    //!
+    //! @param  value       Initialization value
+    Deferred(T const & value)
+        : value_(value)
+        , deferred_(true)
+    {
+    }
+
+    //! Copy constructor.
+    //!
+    //! @param  rhs       Copied value
+    //!
+    //! @note   Copying an unassigned value results in an unassigned value.
+    Deferred(Deferred const & rhs) = default;
+
+    //! Assignment operator.
+    //!
+    //! @param  rhs       Value to assign
+    //!
+    //! @note   Assigning an unassigned value results in an unassigned value.
+    Deferred & operator =(Deferred const & rhs) = default;
+
+    //! Assignment operator.
+    //!
+    //! @param  rhs       Value to assign
+    Deferred & operator =(T const & rhs)
+    {
+        value_    = rhs;
+        deferred_ = true;
+        return *this;
+    }
+
+    //! Conversion operator.
+    //!
+    //! The object is implicitly converted to its value.
+    operator T() const
+    {
+        return value_;
+    }
+
+    //! Returns true if the object has ever been given a value.
+    bool deferred() const
+    {
+        return deferred_;
+    }
+
+    //! Returns the value (or the default value, if never given a value).
+    T value() const
+    {
+        return value_;
+    }
+
+private:
+    T value_       = T();
+    bool deferred_ = false;
+};
+
+#endif // !defined(MISC_DEFERRED_H)
---
+++ test/test-Deferred.cpp
@@ -0,0 +1,81 @@
+#include "Misc/Deferred.h"
+
+#include "gtest/gtest.h"
+
+TEST(DeferredTest, ConstructDefault)
+{
+    EXPECT_FALSE(Deferred<int>().deferred());
+    EXPECT_EQ(Deferred<int>().value(), int());
+}
+
+TEST(DeferredTest, Construct)
+{
+    EXPECT_TRUE(Deferred<int>(1).deferred());
+    EXPECT_EQ(Deferred<int>(1).value(), 1);
+}
+
+TEST(DeferredTest, CopyConstruct)
+{
+    Deferred<int> unassigned;
+    EXPECT_EQ(Deferred<int>(unassigned).deferred(), unassigned.value());
+    EXPECT_EQ(Deferred<int>(unassigned).value(), unassigned.value());
+
+    Deferred<int> assigned(1);
+    EXPECT_EQ(Deferred<int>(assigned).deferred(), assigned.deferred());
+    EXPECT_EQ(Deferred<int>(assigned).value(), assigned.value());
+}
+
+TEST(DeferredTest, Assign)
+{
+    Deferred<int> u;
+    Deferred<int> unassigned;
+    Deferred<int> x(1);
+    Deferred<int> y(2);
+
+    u = unassigned;
+    EXPECT_EQ(u.deferred(), unassigned.deferred());
+    EXPECT_EQ(u.value(), unassigned.deferred());
+    u = x;
+    EXPECT_EQ(u.deferred(), x.deferred());
+    EXPECT_EQ(u.value(), x.value());
+    u = y;
+    EXPECT_EQ(u.deferred(), y.deferred());
+    EXPECT_EQ(u.value(), y.value());
+    u = unassigned;
+    EXPECT_EQ(u.deferred(), unassigned.deferred());
+    EXPECT_EQ(u.value(), unassigned.deferred());
+}
+
+TEST(DeferredTest, AssignValue)
+{
+    Deferred<int> u;
+    Deferred<int> unassigned;
+    Deferred<int> x(1);
+    Deferred<int> y(2);
+
+    u = 1;
+    EXPECT_TRUE(u.deferred());
+    EXPECT_EQ(u.value(), 1);
+    u = 2;
+    EXPECT_TRUE(u.deferred());
+    EXPECT_EQ(u.value(), 2);
+}
+
+TEST(DeferredTest, ConversionOperator)
+{
+    int           u = ~int();
+    Deferred<int> unassigned;
+    Deferred<int> x(1);
+
+    u = (int)x;
+    EXPECT_EQ(u, x.value());
+    u = (int)unassigned;
+    EXPECT_EQ(u, unassigned.value());
+}
+
+int main(int argc, char ** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    int rv = RUN_ALL_TESTS();
+    return rv;
+}
